f464fab700c4373998577e5a1328d763c63f9647
Reachea <reacheasambath@gmail.com>
2020-07-12 21:54:25 +0700

    Finished Footer

--- src/FootNav.tsx
+++ src/FootNav.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Layout, Button, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
+import './FootNav.css';
 
 const { Footer } = Layout;
 
@@ -9,29 +10,90 @@ class FootNav extends Component {
     return (
       <div>
         <Footer>
-          <div className="footNavBar">
-            <Row>
-              <Col span={8} >
-                <div className="footNav">
+          <div className="footNavBar container">
+            <div className="footer" >
+              <Row>
+                <Col span={6} >
+                    <Link to="/about">
+                      <Button type="link" className="foot-nav" >
+                        <span className="span-footer-1" >About</span>
+                      </Button>
+                    </Link>
+                </Col>
+                <Col span={7} ></Col>
+                <Col span={7} ></Col>
+                <Col span={4} >
                   <Link to="/about">
-                    <Button type="link" >About</Button>
+                    <div className="telegram-foot" >
+                      <Button type="link" className="foot-nav" >
+                        <span className="span-footer-1" >Telegram</span>
+                      </Button>
+                    </div>
                   </Link>
-                  <Link to="/works">
-                    <Button type="link" >Works</Button>
-                  </Link>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6} >
+                    <Link to="/works">
+                      <Button type="link" className="foot-nav" >
+                        <span className="span-footer-1" >Works</span>
+                      </Button>
+                    </Link>
+                </Col>
+                <Col span={7} ></Col>
+                <Col span={7} ></Col>
+                <Col span={4} >
                   <Link to="/news">
-                    <Button type="link" >News</Button>
+                    <div className="facebook-foot" >
+                      <Button type="link" className="foot-nav" >
+                        <span className="span-footer-1" >Facebook</span>
+                      </Button>
+                    </div>
                   </Link>
-                  <Link to="/contact">
-                    <Button type="link" >Contact</Button>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6} >
+                    <Link to="/news">
+                      <Button type="link" className="foot-nav" >
+                        <span className="span-footer-1" >News</span>
+                      </Button>
+                    </Link>
+                </Col>
+                <Col span={7} ></Col>
+                <Col span={7} ></Col>
+                <Col span={4} >
+                  <Link to="/news">
+                    <div className="twitter-foot" >
+                      <Button type="link" className="foot-nav" >
+                        <span className="span-footer-1" >twitter</span>
+                      </Button>
+                    </div>
                   </Link>
-                </div>
-              </Col>
-              <Col span={8} >
-              </Col>
-              <Col span={8} >
-              </Col>
-            </Row>
+                </Col>
+              </Row>
+              <Row>
+                <Col span={6} >
+                    <Link to="/contact">
+                      <Button type="link" className="foot-nav" >
+                        <span className="span-footer-1" >Contact</span>
+                      </Button>
+                    </Link>
+                </Col>
+                <Col span={6} ></Col>
+                <Col span={6} ></Col>
+                <Col span={6} >
+                </Col>
+              </Row>
+              <Row>
+                <Col span={10} ></Col>
+                <Col span={5} ></Col>
+                <Col span={5} ></Col>
+                <Col span={4} >
+                  <div className="footer-logo" ></div>
+                </Col>
+              </Row>
+            </div>
           </div>
         </Footer>
       </div>
